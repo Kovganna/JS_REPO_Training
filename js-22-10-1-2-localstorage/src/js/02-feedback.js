1b2d20214e8c4fd0ced1a954e11a4928d244ef48
Ganna <annakovalenko168@gmail.com>
2021-09-08 11:24:04 +0300

    modul 10 task1

--- js-22-10-1-2-localstorage/src/js/02-feedback.js
+++ js-22-10-1-2-localstorage/src/js/02-feedback.js
@@ -1,15 +1,15 @@
-import throttle from 'lodash.throttle';
+// import throttle from 'lodash.throttle';
 import '../css/common.css';
 import '../css/feedback-form.css';
 // var throttle = require('lodash.throttle');
 
-const STORAGE_KEY = 'feedback-msg';
+// const STORAGE_KEY = 'feedback-msg';
 
-const refs = {
-  form: document.querySelector('.js-feedback-form'),
-  // textarea: document.querySelector('.js-feedback-form  textarea'),
-  input: document.querySelector('.js-feedback-form  input'),
-};
+// const refs = {
+//   form: document.querySelector('.js-feedback-form'),
+//   // textarea: document.querySelector('.js-feedback-form  textarea'),
+//   input: document.querySelector('.js-feedback-form  input'),
+// };
 
 // refs.form.addEventListener('submit', onFormSubmit);
 // refs.textarea.addEventListener('input', throttle(onTextareaInput, 200));
@@ -60,50 +60,73 @@ const refs = {
 //  * - Если там что-то было, обновляем DOM
 //  */
 
-function populateTextarea () {
- const savedMessage = localStorage.getItem('STORAGE_KEY');
+// function populateTextarea () {
+//  const savedMessage = localStorage.getItem('STORAGE_KEY');
  
- if(savedMessage) {
-  console.log(savedMessage);
-  refs.textarea.value = savedMessage;
- }
-}
+//  if(savedMessage) {
+//   console.log(savedMessage);
+//   refs.textarea.value = savedMessage;
+//  }
+// }
 
-function populateInput () {
-  const savedName = localStorage.getItem('feedback-name');
+// function populateInput () {
+//   const savedName = localStorage.getItem('feedback-name');
 
-   if(savedName) {
-   console.log(savedName);
-   refs.input.value = savedName;
-  }
- }
+//    if(savedName) {
+//    console.log(savedName);
+//    refs.input.value = savedName;
+//   }
+//  }
 
 // Домой
 // сделать так чтобы сохраняло не только сообщение но и имя,
 //  и все в одном обьекте
 
+const formData = {}
+
+const refs = {
+  form: document.querySelector('.js-feedback-form'),
+  textarea: document.querySelector('.js-textarea'),
+  input: document.querySelector('.js-input')
+}
+
+populateTextarea();
+populateInput();
 
 refs.form.addEventListener('input', e => {
   e.preventDefault();
 
-  const formData = new FormData(e.currentTarget);
-
+  formData[e.target.name] = e.target.value;
+  const strFormData = JSON.stringify(formData);
+  localStorage.setItem('feedback', strFormData);
   
-  formData.forEach((value, name) => {
-   
-    console.log("onFormSubmit -> name", name)    
-    console.log("onFormSubmit -> value", value)
-
-    
-
-  })
-//     //  console.log(e.target.name);
-//     //  console.log(e.target.value);
-
-//      formData[e.target.name] = e.target.value;
-
   console.log(formData);
 });
 
+refs.form.addEventListener('submit', e => {
+  e.preventDefault();
+  e.currentTarget.reset();//приводит форму в изначальное состояние
+  
+  localStorage.removeItem('feedback', strFormData);
+})
 
+function populateTextarea () {
+ const savedMessage = localStorage.getItem('feedback');
+ const parsedMessage = JSON.parse(savedMessage);
+
+ if(parsedMessage) {
+  console.log(parsedMessage);
+  refs.textarea.value = parsedMessage.message;
+ }
+}
+
+function populateInput () {
+  const savedName = localStorage.getItem('feedback');
+  const parsedName = JSON.parse(savedName);
+
+   if(parsedName) {
+   console.log(parsedName);
+   refs.input.value = parsedName.name;
+  }
+ }
 
